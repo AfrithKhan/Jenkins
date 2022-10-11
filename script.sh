9946490364af05576401aab07964779cdd888a6d
Ubuntu <ubuntu@ip-172-31-0-68.ap-southeast-1.compute.internal>
2022-10-11 18:32:08 +0000

    docker

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 ssh ubuntu@172.31.0.68 <<EOF
     cd /var/Nodejs/Jenkins
     git pull origin master
-    npm install
-    pm2 restart index.js
+    docker build -t nodejs:$1 .
+    docker stop nodejs
+    docker run -d -p 3000:3000 nodejs:$1
     exit
 EOF
