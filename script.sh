3bb02b949a5dc9b3e5c5769a394dcc02cb67d76f
Ubuntu <ubuntu@ip-172-31-0-68.ap-southeast-1.compute.internal>
2022-10-11 13:04:29 +0000

    pipeline

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-ssh -tt -o StrictHostKeyChecking=no root@172.31.9.147
-cd /home/ubuntu/Nodejs/Jenkins
-git pull origin master
-npm install
-pm2 stop index.js
-pm2 start index.js
+ssh ubuntu@172.31.0.68 <<EOF
+    cd /var/Nodejs/Jenkins
+    git pull origin master
+    curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
+    . ~/.nvm/nvm.sh
+    nvm install v10.11.0
+    npm install
+    npm install -g nodemon pm2
+    pm2 restart ecosystem.config.js
+    exit
+EOF
