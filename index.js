e6d1d3d16b7a329e7336715820599ce630da7a7f
Ubuntu <ubuntu@ip-172-31-0-68.ap-southeast-1.compute.internal>
2022-10-11 13:44:41 +0000

    pipeline

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 app.get('/', (req, res)=> {
-res.send('Hello Afrith-new');
+res.send('Hello Afrith');
 });
 
 app.post('/getIndexes', (req, res)=> {
